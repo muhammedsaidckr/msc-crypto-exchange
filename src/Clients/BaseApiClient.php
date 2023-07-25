06ac5c18bf9f3bc1f3ce62ca83a7c21f9ea5924b
muhammedsaidckr <muhammedsaidckr@gmail.com>
2023-07-26 00:27:17 +0300

    RestApiClient wip

--- src/Clients/BaseApiClient.php
+++ src/Clients/BaseApiClient.php
@@ -2,15 +2,24 @@
 
 namespace Msc\MscCryptoExchange\Clients;
 
-use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Stream;
 use Illuminate\Support\Facades\Log;
+use Msc\MscCryptoExchange\Authentication\ApiCredentials;
 use Msc\MscCryptoExchange\Contracts\BaseApiClient as BaseApiClientAlias;
+use Msc\MscCryptoExchange\Helpers\JsonHelper;
+use Msc\MscCryptoExchange\Objects\CallResult;
+use Msc\MscCryptoExchange\Objects\CallResultWithData;
+use Msc\MscCryptoExchange\Objects\Errors\DeserializeError;
+use Msc\MscCryptoExchange\Objects\Options\ApiOptions;
+use Msc\MscCryptoExchange\Objects\Options\ExchangeOptions;
 
 abstract class BaseApiClient implements BaseApiClientAlias
 {
+    protected $log;
+
     /**
      * The base address for this API client
      *
@@ -26,26 +35,86 @@ abstract class BaseApiClient implements BaseApiClientAlias
     protected $httpClient;
 
     /**
+     * @var \Msc\MscCryptoExchange\Authentication\AuthenticationProvider
+     */
+    protected $authenticationProvider;
+
+    /**
+     * @var \Msc\MscCryptoExchange\Objects\Options\RestApiOptions $apiOptions
+     */
+    protected $apiOptions;
+
+    /**
+     * @var \Msc\MscCryptoExchange\Objects\Options\ExchangeOptions $clientOptions
+     */
+    protected $clientOptions;
+
+    /**
+     * @var bool $outputOriginalData
+     */
+    public bool $outputOriginalData;
+
+    /**
      * Create a new instance of the base API client.
      *
-     * @param string $baseAddress The base address for this API client.
+     * @param  string  $baseAddress  The base address for this API client.
      */
-    public function __construct(string $baseAddress)
-    {
+    public function __construct(
+        Log $log,
+        bool $outputOriginalData,
+        ?ApiCredentials $apiCredentials,
+        string $baseAddress,
+        ExchangeOptions $clientOptions,
+        ApiOptions $apiOptions
+    ) {
+        $this->log = $log;
+        $this->clientOptions = $clientOptions;
+        $this->apiOptions = $apiOptions;
+        $this->outputOriginalData = $outputOriginalData;
         $this->baseAddress = $baseAddress;
-        $this->httpClient = new Client([
-            'base_uri' => $this->baseAddress,
-            'headers' => [
-                'Content-Type' => 'application/json',
-            ],
-        ]);
+//        ]);
+        $this->authenticationProvider = self::createAuthenticationProvider($apiCredentials);
+    }
+
+    /**
+     * @param  \Msc\MscCryptoExchange\Authentication\ApiCredentials  $credentials
+     * @return mixed
+     */
+    protected abstract function createAuthenticationProvider(ApiCredentials $credentials);
+
+    /**
+     * Deserialize a stream into an object
+     *
+     * @param $stream
+     * @param  int|null  $requestId
+     * @param  int|null  $elapsedMilliseconds
+     * @return CallResult
+     */
+    protected function deserialize($stream, ?int $requestId = null, ?int $elapsedMilliseconds = null): CallResult
+    {
+        $serializer = $this->defaultSerializer();
+        $data = null;
+
+        try {
+            $reader = new Stream($stream);
+            if ($this->outputOriginalData) {
+                $data = $reader->read($reader->getSize());
+                $this->log->debug("Response recived: ".! is_null($elapsedMilliseconds) ? "in $elapsedMilliseconds" : "Original data: $data");
+                return new CallResultWithData(json_decode($data), $data);
+            }
+
+            return new CallResultWithData(json_decode($data));
+        } catch (\Exception $e) {
+            $this->log->error($e->getMessage());
+            return new CallResult(new DeserializeError("Deserialize unknown exception: ".$e->getMessage(), $data));
+        }
     }
 
     /**
      * Perform a GET request to the API.
      *
-     * @param string $endpoint The API endpoint to request.
-     * @param array $queryParams Query parameters for the request.
+     * @param  string  $endpoint  The API endpoint to request.
+     * @param  array  $queryParams  Query parameters for the request.
      * @return mixed
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
@@ -54,7 +123,7 @@ abstract class BaseApiClient implements BaseApiClientAlias
         try {
             $response = $this->httpClient->get($endpoint, ['query' => $queryParams]);
             return $this->handleResponse($response);
-        } catch (ClientException | RequestException | GuzzleException $exception) {
+        } catch (ClientException|RequestException|GuzzleException $exception) {
             return $this->handleException($exception);
         }
     }
@@ -62,8 +131,8 @@ abstract class BaseApiClient implements BaseApiClientAlias
     /**
      * Perform a POST request to the API.
      *
-     * @param string $endpoint The API endpoint to request.
-     * @param array $data Data to send in the request body.
+     * @param  string  $endpoint  The API endpoint to request.
+     * @param  array  $data  Data to send in the request body.
      * @return mixed
      */
     protected function post(string $endpoint, array $data = [])
@@ -71,7 +140,7 @@ abstract class BaseApiClient implements BaseApiClientAlias
         try {
             $response = $this->httpClient->post($endpoint, ['json' => $data]);
             return $this->handleResponse($response);
-        } catch (ClientException | RequestException | GuzzleException $exception) {
+        } catch (ClientException|RequestException|GuzzleException $exception) {
             return $this->handleException($exception);
         }
     }
@@ -79,7 +148,7 @@ abstract class BaseApiClient implements BaseApiClientAlias
     /**
      * Handle the API response.
      *
-     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param  \Psr\Http\Message\ResponseInterface  $response
      * @return mixed
      */
     protected function handleResponse($response)
@@ -91,12 +160,20 @@ abstract class BaseApiClient implements BaseApiClientAlias
     /**
      * Handle API exceptions.
      *
-     * @param \GuzzleHttp\Exception\GuzzleException $exception
+     * @param  \GuzzleHttp\Exception\GuzzleException  $exception
      * @return mixed
      */
     protected function handleException($exception)
     {
         Log::error($exception->getMessage());
         return null;
+    }
+
+    private static function defaultSerializer()
+    {
+        return JsonHelper::encode([
+            "dateTimeZoneHandling" => 'UTC',
+            "culture" => app()->getLocale(),
+        ]);
     }
 }
--- src/Clients/RestApiClient.php
+++ src/Clients/RestApiClient.php
@@ -9,41 +9,60 @@ use Msc\MscCryptoExchange\Contracts\DateTimeContract;
 use Msc\MscCryptoExchange\Contracts\Request;
 use Msc\MscCryptoExchange\Contracts\RestApiClient as RestApiClientAlias;
 use Msc\MscCryptoExchange\Objects\CallResult;
+use Msc\MscCryptoExchange\Objects\CallResultWithData;
+use Msc\MscCryptoExchange\Objects\Errors\NoApiCredentialsError;
 use Msc\MscCryptoExchange\Objects\Options\RestApiOptions;
 use Msc\MscCryptoExchange\Objects\Options\RestExchangeOptions;
 use Msc\MscCryptoExchange\Objects\TimeSyncInfo;
 use Msc\MscCryptoExchange\Objects\WebCallResult;
 use Msc\MscCryptoExchange\Requests\RequestFactory;
 use Psr\Http\Message\UriInterface;
+use Illuminate\Support\Facades\Log;
 
 abstract class RestApiClient extends BaseApiClient implements RestApiClientAlias
 {
     protected RequestFactory $requestFactory;
     protected ?int $totalRequestsMade;
     protected ?array $standardRequestHeaders;
-    protected array $rateLimiters;
-    protected RestExchangeOptions $clientOptions;
-    protected RestApiOptions $apiOptions;
-    protected Logger $log;
+
+    /**
+     * @var \Msc\MscCryptoExchange\Objects\Options\RestExchangeOptions $clientOptions
+     */
+    protected $clientOptions;
+
+    /**
+     * @var \Msc\MscCryptoExchange\Objects\Options\RestApiOptions $apiOptions
+     */
+    protected $apiOptions;
+
+    /**
+     * @var \Illuminate\Log\Logger $log
+     */
+    protected $log;
 
     abstract public function getTimeSyncInfo(): ?TimeSyncInfo;
 
     abstract public function getTimeOffset(): ?DateTimeContract;
 
     public function __construct(
-        string $baseAddress,
-        Logger $log,
+        Log $log,
         ?Client $httpClient,
+        string $baseAddress,
         RestExchangeOptions $options,
         RestApiOptions $apiOptions
     ) {
-        parent::__construct($baseAddress);
+        parent::__construct(
+            $log,
+            $apiOptions->getOutputOriginalData() ?? $options->outputOriginalData,
+            $apiOptions->getApiCredentials() ?? $options->apiCredentials,
+            $baseAddress,
+            $options,
+            $apiOptions);
         $this->log = $log;
         $this->requestFactory = new RequestFactory();
         $this->totalRequestsMade = 0;
         $this->clientOptions = $options;
         $this->apiOptions = $apiOptions;
-        $this->rateLimiters = $apiOptions->getRateLimiters();
         $this->requestFactory->configure($options->requestTimeout, $httpClient);
     }
 
@@ -55,7 +74,7 @@ abstract class RestApiClient extends BaseApiClient implements RestApiClientAlias
         bool $signed = false,
         int $requestWeight = 1,
         ?array $additionalHeaders = null,
-        bool $ignoreRatelimit = false
+        bool $ignoreRatelimit = true
     ): WebCallResult {
         $currentTry = 0;
 
@@ -77,9 +96,44 @@ abstract class RestApiClient extends BaseApiClient implements RestApiClientAlias
 //        string $arraySerialization = null,
         int $requestWeight = 1,
         ?array $additionalHeaders = null,
-        bool $ignoreRatelimit = false
+        bool $ignoreRatelimit = true
     ): CallResult {
-        // TODO: Implement the method.
+        $requestId = $this->nextId();
+
+        if ($signed) {
+            $syncTask = $this->syncTimeAsync();
+            $timeSyncInfo = $this->getTimeSyncInfo();
+
+            if ($timeSyncInfo !== null) {
+                // Initially with first request we'll need to wait for the time syncing, if it's not the first request we can just continue
+//                $syncTimeResult = $syncTask->wait();
+//                if (! $syncTimeResult) {
+//                    $this->log->debug("[$requestId] Failed to sync time, aborting request: ".$syncTimeResult->getError());
+//                    return $syncTimeResult->asIRequest();
+//                }
+            }
+        }
+
+        if ($signed) {
+            $this->log->warning("[{requestId}] Request {uri.AbsolutePath} failed because no ApiCredentials were provided");
+            return new CallResult(new NoApiCredentialsError());
+        }
+
+        $this->log->info("[{requestId}] Creating request for ".$uri);
+        $request = ConstructRequest(uri, method,
+                parameters ?.OrderBy(p => p.Key).ToDictionary(p => p.Key, p => p.Value), signed, paramsPosition, arraySerialization ?? this.arraySerialization, requestId, additionalHeaders);
+
+            string ? paramString = "";
+            if (paramsPosition == HttpMethodParameterPosition.InBody)
+                paramString = $" with request body '{request.Content}'";
+
+            var headers = request.GetHeaders();
+            if (headers.Any())
+                paramString += " with headers " + string.Join(", ", headers.Select(h => h.Key + $"=[{string.Join(",", h.Value)}]"));
+
+            $totalRequestMode++;
+            Log::info("[{$requestId}] Sending {method}{(signed ? " $signed" : "")} request to {request.Uri}{paramString ?? " "}");
+            return new CallResultWithData($reqest);
     }
 
     protected function getResponseAsync(
@@ -89,15 +143,89 @@ abstract class RestApiClient extends BaseApiClient implements RestApiClientAlias
     ): WebCallResult {
     }
 
+    /**
+     * @throws \Exception
+     */
     protected function constructRequest(
-        UriInterface $uri,
+        Uri $uri,
         string $method,
         ?array $parameters,
         bool $signed,
+        string $parameterPosition,
         int $requestId,
         ?array $additionalHeaders
     ): Request {
-        // TODO: Implement the method.
+        $parameters ??= [];
+        $uriParameters = [];
+        $bodyParameters = [];
+        $headers = [];
+        $additionalHeaders ??= [];
+
+//        if ($parameterPosition == 'URI') {
+//            foreach ($parameters as $key => $value) {
+//                $uri = $uri->withQuery($parameters);
+//            }
+//        }
+
+        $authenticationProvider = $this->authenticationProvider;
+        if ($authenticationProvider != null) {
+            try {
+                $authenticationProvider->authenticateRequest(
+                    $this,
+                    $uri,
+                    $method,
+                    $parameters,
+                    $signed,
+                    $uriParameters,
+                    $bodyParameters,
+                    $headers
+                );
+            } catch (\Exception $ex) {
+                throw new \Exception("Failed to authenticate request, make sure your API credentials are correct", $ex);
+            }
+        }
+
+        // Sanity check
+        foreach ($parameters as $key => $value) {
+            if (! isset($uriParameters[$key]) && ! isset($bodyParameters[$key])) {
+                throw new \Exception("Missing parameter $key after authentication processing. AuthenticationProvider implementation ".
+                    "should return provided parameters in either the uri or body parameters output");
+            }
+        }
+
+        // Add the auth parameters to the uri, start with a new URI to be able to sort the parameters including the auth parameters
+        $uri = $uri->setParameters($uriParameters, $arraySerialization);
+
+        $request = $this->requestFactory->create($method, $uri, $requestId);
+        $request->accept = Constants::JSON_CONTENT_HEADER;
+
+        foreach ($headers as $key => $value) {
+            $request->withAddedHeader($key, $value);
+        }
+
+        foreach ($additionalHeaders as $key => $value) {
+            $request->withAddedHeader($key, $value);
+        }
+
+        if ($this->standardRequestHeaders != null) {
+            foreach ($this->standardRequestHeaders as $key => $value) {
+                // Only add it if it isn't overwritten
+                if (! isset($additionalHeaders[$key])) {
+                    $request->withAddedHeader($key, $value);
+                }
+            }
+        }
+
+        if ($parameterPosition == 'BODY') {
+            $contentType = $this->requestBodyFormat == RequestBodyFormat::JSON ? Constants::JSON_CONTENT_HEADER : Constants::FORM_CONTENT_HEADER;
+            if (! empty($bodyParameters)) {
+                $this->writeParamBody($request, $bodyParameters, $contentType);
+            } else {
+                $request->setContent($this->requestBodyEmptyContent, $contentType);
+            }
+        }
+
+        return $request;
     }
 
     protected function validateJson(string $data): WebCallResult
